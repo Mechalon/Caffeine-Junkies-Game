b45731650a2aace2a44269f6f14daecf995b4264
ffleck <forrestfleck@my.unt.edu>
2012-04-17 13:21:48 -0500

    Added CheckPoint Class
    Needs to be revised once player class can be referenced...maybe.

---
+++ Software Development I/Software Development I/CheckPoint.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+
+/*
+ * Revise UpdateLastCheckPoint
+ */
+namespace Software_Development_I
+{
+    class CheckPoint
+    {
+        //list of all checkpoints in game/level
+        public static List<CheckPoint> list = new List<CheckPoint> { };
+
+        //checkpoint to return on death
+        public static CheckPoint last;
+
+
+        //values to return to after death
+        private int level;
+        private int xPos;
+        private int yPos;
+
+        public CheckPoint(int level, int xPos, int yPos)
+        {
+            this.level = level;
+            this.xPos = xPos;
+            this.yPos = yPos;
+            list.Add(this);
+        } //end CheckPoint
+
+        public static void UpdateLastCheckPoint()
+        {
+            for (int i = 0; i < list.Count(); i++)
+                if (list[i].xPos < Player.position.x < list[i].xPos+5)
+                    last = list[i];
+        } //end UpdateLastCheckPoint
+
+    } //end CheckPoint
+} //end
